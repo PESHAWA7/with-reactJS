d484112883f0404683ad86b4dcdc690c365a0380
peshawa omer mohemmed <155902765+PESHAWA7@users.noreply.github.com>
2024-08-31 11:04:35 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <h1>hellow worddd</h1>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://w00gz.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
